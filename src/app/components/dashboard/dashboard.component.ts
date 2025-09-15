ac8e0c03225681dfde09ec8e118344173c84708a
Rolando <rolyan66@gmail.com>
2025-09-14 20:43:58 -0400

    Se realizo la adición de funcionalidad ademas que se finalizo el proyecto

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,16 +1,44 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ClimaService } from '../../services/clima.service';
+import { NgIf,CommonModule  } from '@angular/common';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
   urlImagen='https://cdn-icons-png.flaticon.com/512/1116/1116453.png';
   ciudad:string='';
+  query=false;
+  loading:boolean=false;
+  temperatura:number=0;
+  humedad=0;
+  clima='';
+  error:boolean=false;
+  constructor(private _serviceClima:ClimaService){}
   obtenerClima(){
-    console.log(this.ciudad);
+    this.query=false;
+    this.loading=true;
+    this._serviceClima.getClima(this.ciudad).subscribe(data=>{
+      this.query=true;
+      this.loading=false;
+      this.temperatura=data.main.temp - 273;
+      this.humedad=data.main.humidity;
+      this.clima=data.weather[0].main;
+    },error=>{
+      this.mostrarError();
+    })
+    
   }
+  mostrarError(){
+    this.error=true;
+    this.loading=false;
+    setTimeout(()=>{
+      this.error=false;
+      this.ciudad='';
+    },3000)
+}
 }
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
-
-  constructor() { }
+  url='https://api.openweathermap.org/data/2.5/weather?appid=';
+  key='c664eb56b6185cba4d74a3c74e801684';
+  constructor(private httpClient:HttpClient) { }
+  getClima(ciudad:string):Observable<any>{
+    const URL=this.url+this.key+'&q='+ciudad;
+    return this.httpClient.get(URL);
+  }
 }
